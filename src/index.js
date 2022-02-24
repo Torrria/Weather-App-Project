c21b0e7782d3fca4491ef83e6e8d87e858e2b878
Torrria <95705556+Torrria@users.noreply.github.com>
2022-02-24 10:56:25 -0500

    Updated the link for icon

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://fontawesome.com/v6/icons?c=${response.data.weather[0].icon}&s=duotone%2Clight`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
